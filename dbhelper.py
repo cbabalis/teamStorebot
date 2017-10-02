c5f83ea88f46c7b3a2cfb3b72cf89c8709ccda13
Babis Babalis <babisbabalis@gmail.com>
2017-10-02 07:15:11 +0300

    new functionality

--- dbhelper.py
+++ dbhelper.py
@@ -22,6 +22,7 @@ class DBHelper:
         self.conn.commit()
 
     def delete_item(self, item_text, owner):
+        #TODO have a url in item, which the user follows it just before leaving
         stmt = "DELETE FROM items WHERE description = (?) AND owner = (?)"
         args = (item_text, owner)
         self.conn.execute(stmt, args)

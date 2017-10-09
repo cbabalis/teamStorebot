2579845db86121f8f7a64b9d0ec675ac28757d6e
Babis Babalis <babisbabalis@gmail.com>
2017-10-09 18:21:59 +0300

    new changes

--- mybot.py
+++ mybot.py
@@ -6,35 +6,37 @@ class TodoBot(telegram.Bot):
     def __init__(self):
         super(TodoBot, self).__init__(token=token)
         self.commands = ['/add', '/pop', '/help', '/show']
+        self.items = []
 
     def add_item(self, text):
-        pass
+        self.items.append(text)
 
     def pop_item(self):
-        return None
+        return self.items.pop()
 
     def help(self):
         # print out all commands and description
-        pass
+        return "All stuff is pretty forward"
 
     def show_all(self):
         # show all items
-        pass
+        return self.items
 
     def process(self, text):
+        message = ""
         # if text starts with any command, then identify it
         if text.startswith("/add"):
             self.add_item(text.split("/add")[-1])
         elif text.startswith("/pop"):
-            self.pop_item()
+            message = self.pop_item()
         elif text.startswith("/help"):
-            self.help()
+            message = self.help()
         elif text.startswith("/show_all"):
-            self.show_all()
+            message = self.show_all()
         else:
             continue
 
-
-    def process(self, update):
-        return self.sendMessage(chat_id=update.message.chat.id, text=update.message)
-        pass
+        if message:
+            return self.sendMessage(chat_id=update.message.chat.id, text=update.message.text)
+        else:
+            continue
